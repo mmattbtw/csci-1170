95f2f898d14f7ce397ae2e32cc0d37b1c8565192
mmattbtw <matt@mmatt.net>
2024-04-18 10:17:08 -0500

    mode median mean 4/18/2024

---
+++ mode_median_mean/mode_median_mean.py
@@ -0,0 +1,32 @@
+def main():
+    file = open("steps.txt")
+    steps = []
+    for line in file:
+        steps.append(int(line.strip()))
+
+    print("Median:", Median(steps), "Mean:", Mean(steps), "Mode:", Mode(steps))
+
+def Median(steps):
+    numbers = steps[:]
+    numbers.sort()
+    
+    if len(numbers) % 2 == 0:
+        return numbers[len(numbers) // 2]
+    else:
+        return numbers[len(numbers) // 2 - 1]
+
+def Mean(steps):
+    return sum(steps) / len(steps)
+
+def Mode(steps):
+    max_count = 0
+    mode = 0
+    for number in steps:
+        if steps.count(number) > max_count:
+            max_count = steps.count(number)
+            mode = number
+    
+    return mode
+
+if __name__ == "__main__":
+    main()
